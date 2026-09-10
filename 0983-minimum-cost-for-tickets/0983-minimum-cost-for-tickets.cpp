9298dabab75932f059b920b0e10954788fa791c6
Ankitmodi989 <ankitmodi2428@gmail.com>
2026-09-10 22:55:30 +0530

    Time: 3 ms (26.5%), Space: 13.3 MB (58.73%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,40 +1,31 @@
+#include <algorithm>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
-    int n;
-    vector<int> dp;
-    long long ticket(int i, vector<int>& days, vector<int>& costs) {
-        if (i == n) {
-            return 0;
-        }
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        int mini = INT_MAX;
-
-        int t1 = costs[0] + ticket(i + 1, days, costs);
-        mini = min(mini, t1);
-
-        int j = i + 1;
-        while (j < n && days[j] < (days[i] + 7)) {
-            j++;
-        }
-        int t2 = costs[1] + ticket(j, days, costs);
-        mini = min(mini, t2);
-
-        while (j < n && days[j] < (days[i] + 30)) {
-            j++;
-        }
-        int t3 = costs[2] + ticket(j, days, costs);
-        mini = min(mini, t3);
-        dp[i] = mini;
-
-        return dp[i];
-    }
-
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        n = days.size();
-        dp.assign(n, -1);
-        return ticket(0, days, costs);
+        unordered_set<int> travelDays(days.begin(), days.end());
+        int lastDay = days.back();
+        vector<int> dp(lastDay + 1, 0);
+
+        //hnn aa jao dekh lo kese solve kiya h mene
+
+        for (int i = 1; i <= lastDay; i++) {
+            if (travelDays.find(i) == travelDays.end()) {
+                dp[i] = dp[i - 1];
+                continue;
+            }
+
+            int pass1 = dp[i - 1] + costs[0];
+            int pass7 = dp[max(0, i - 7)] + costs[1];
+            int pass30 = dp[max(0, i - 30)] + costs[2];
+
+            dp[i] = min({pass1, pass7, pass30});
+        }
+
+        return dp[lastDay];
     }
 };
